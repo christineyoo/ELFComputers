072b864792ddb7bdcab4628888290d61b83b1837
Christine Yoo <christinecho.s@gmail.com>
2021-02-18 15:44:07 -0800

    edited features in ListItem.js

--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 const USCurrencyFormat = new Intl.NumberFormat("en-US", {
   style: "currency",
@@ -14,7 +14,7 @@ function CartItem(props) {
     return (
       <>
         <div className="summary__option" key={featureHash}>
-          <div className="summary__option__label">{feature} </div>
+          <div className="summary__option__label" >{feature} </div>
           <div className="summary__option__value">{selectedOption.name}</div>
           <div className="summary__option__cost">
             {USCurrencyFormat.format(selectedOption.cost)}
--- src/ListItem/ListItem.js
+++ src/ListItem/ListItem.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import Features from "../Features.js";
-
-// Creates a string that is safe to use in both URLs and html attributes
 import slugify from "slugify";
 
 // This object converts numbers into US dollar values
@@ -11,57 +9,28 @@ const USCurrencyFormat = new Intl.NumberFormat("en-US", {
 });
 
 class ListItem extends Component {
-  state = {
-    allFeatures: {
-      Processor: [
-        {
-          name: "17th Generation Intel Core HB (7 Core with donut spare)",
-          cost: 700,
-        },
-        {
-          name: "Professor X AMD Fire Breather with sidewinder technology",
-          cost: 1200,
-        },
-      ],
-      "Operating System": [
-        {
-          name: "Ubuntu Linux 16.04",
-          cost: 200,
-        },
-        {
-          name: "Bodhi Linux",
-          cost: 300,
-        },
-      ],
-      "Video Card": [
-        {
-          name: "Toyota Corolla 1.5v",
-          cost: 1150.98,
-        },
-        {
-          name: "Mind mild breeze 2000",
-          cost: 1345,
-        },
-      ],
-      Display: [
-        {
-          name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
-          cost: 1500,
-        },
-        {
-          name: '15.3" HGTV (3840 x 2160) Home makeover edition',
-          cost: 1400,
-        },
-      ],
-    },
-  };
-
   render() {
-    console.log(Features);
-    const features = Object.keys(this.state.allFeatures).map(
-      (feature) => feature
-    );
-    return <div>Hello</div>;
+    const features = Object.keys(Features).map((feature) => {
+      Features[feature].map((item) => {
+        const itemHash = slugify(JSON.stringify(item));
+        return (
+          <div key={itemHash} className="feature__item">
+            <input
+              type="radio"
+              id={itemHash}
+              className="feature__option"
+              name={slugify(feature)}
+              checked={item.name === this.props.selected[feature].name}
+              onChange={(e) => this.props.OnUpdateFeature(feature, item)}
+            />
+            <label htmlFor={itemHash} className="feature__label">
+              {item.name} ({USCurrencyFormat.format(item.cost)})
+            </label>
+          </div>
+        );
+      });
+    });
+    return <>{features}</>;
   }
 }
 
