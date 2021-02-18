1fe8fabd9152f108a614d4f0836ad2008d67457d
Christine Yoo <christinecho.s@gmail.com>
2021-02-17 20:23:43 -0800

    implemented CartItem component

--- src/CartList/CartList.js
+++ src/CartList/CartList.js
@@ -1,33 +1,13 @@
 import React, { Component } from "react";
 import TotalPrice from "../TotalPrice/TotalPrice";
-
-const USCurrencyFormat = new Intl.NumberFormat("en-US", {
-  style: "currency",
-  currency: "USD",
-});
+import CartItem from "../CartItem/CartItem";
 
 class CartList extends Component {
   render() {
-    // Your cart section
-    const summary = Object.keys(this.props.selected).map((feature, idx) => {
-      const featureHash = feature + "-" + idx;
-      const selectedOption = this.props.selected[feature];
-
-      return (
-        <div className="summary__option" key={featureHash}>
-          <div className="summary__option__label">{feature} </div>
-          <div className="summary__option__value">{selectedOption.name}</div>
-          <div className="summary__option__cost">
-            {USCurrencyFormat.format(selectedOption.cost)}
-          </div>
-        </div>
-      );
-    });
-
     return (
       <section className="main__summary">
         <h2>Your cart</h2>
-        {summary}
+        <CartItem selected={this.props.selected} />
         <TotalPrice selected={this.props.selected} />
       </section>
     );
